43ad58dd1abb5419935b43d9fd33f223148e9803
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-18 09:06:13 -0300

    test: ensure SignUpController returns an error if no test is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,17 +1,31 @@
-import { SignUpController } from "./signup"
+import { SignUpController } from './signup'
 
 describe('SignUp controller', () => {
   test('Should return 400 if no name is provided ', () => {
-      const sut = new SignUpController()
-      const httpRequest = {
-        body: {        
-          email: 'any_email@mail.com',
-          password: 'any_password',
-          password_confirmation: 'any_password',
-        }
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        password_confirmation: 'any_password'
       }
-      const httpResponse = sut.handle(httpRequest)
-      expect(httpResponse.statusCode).toBe(400)
-      expect(httpResponse.body).toEqual(new Error('Missing Param: name'))
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided ', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })

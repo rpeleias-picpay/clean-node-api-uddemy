f2968548b2b67189ad096c589b67a7d5351727cb
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-20 14:02:56 -0300

    refactor: move protocols to data

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,6 +1,6 @@
 import { AccountModel } from '../../../domain/models/account'
 import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
-import { Encrypter } from './protocols/encrypter'
+import { Encrypter } from '../../protocols/encrypter'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
@@ -11,6 +11,6 @@ export class DbAddAccount implements AddAccount {
 
   async add (account: AddAccountModel): Promise<AccountModel> {
     await this.encrypter.encrypt(account.password)
-    return await new Promise(resolve => resolve(null))
+    return await new Promise(resolve => resolve('null'))
   }
 }

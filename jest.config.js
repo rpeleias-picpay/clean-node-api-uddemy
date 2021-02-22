a7060f3b6cd2f9e3c64a3b9e2944b9a3e1bdab1d
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-22 14:11:54 -0300

    chore: addedmore jest ignore tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'],
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/presentation/protocols/index.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',

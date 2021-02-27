6c3a3cdaee26f9fb8ba6026c96823b584e1f2336
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-27 17:49:18 -0300

    test: ensure ValidationComposite returns an error if any validation fails

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -14,6 +14,7 @@ export const makeSignUpController = (): Controller => {
   const dbAddAccount = new DbAddAccount(bCryptAdapter, accountMongoRepository)
   const validationComposite = makeSignUpValidation()
   const signUpController = new SignUpController(dbAddAccount, validationComposite)
+
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }

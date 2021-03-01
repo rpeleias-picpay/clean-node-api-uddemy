fb432f9a10a7fa3e454a933bdf88fd0628b8e422
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-01 09:46:44 -0300

    feat: add LoginFactory

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -4,8 +4,8 @@ import { Controller, HttpRequest, HttpResponse, Authentication } from './login-c
 
 export class LoginController implements Controller {
   constructor (
-    private readonly validation: Validation,
-    private readonly authentication: Authentication
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {

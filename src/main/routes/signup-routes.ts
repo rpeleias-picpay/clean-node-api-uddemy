0227de6a0cc576569a97989ef566107a51517a03
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-01 09:30:38 -0300

    refactor: move EmailValdiatorAdapter to the main layer

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { makeSignUpController } from '../factories/signup/signup-factory'
-import { adaptRoute } from '../adapters/express-route-adapters'
+import { adaptRoute } from '../adapters/express/express-route-adapters'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises

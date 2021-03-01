7950875fc0851ae1359e33afc792925f6f540040
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-01 09:36:48 -0300

    refactor: rename signup routes with login routes

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL)
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
+
+beforeEach(async () => {
+  const accountCollection = await MongoHelper.getCollection('accounts')
+  await accountCollection.deleteMany({})
+})
+describe('POST /signup', () => {
+  test('Should return 200 on  signup', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Rodrigo',
+        email: 'rodrigo.peleias@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})

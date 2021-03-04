6ffd58c07199589a85ceb4ca5af1ed1680d158ad
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-04 09:43:57 -0300

    test: ensure SignUpcontroller returns an accessToken on success

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -92,7 +92,7 @@ describe('SignUp controller', () => {
   test('Should return 200 if valid data is provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
-    expect(httpResponse).toEqual(ok(makeFakeAccount()))
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
   })
 
   test('Should call Vaidation with correct value', async () => {

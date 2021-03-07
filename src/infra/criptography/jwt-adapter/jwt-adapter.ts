abfe9b504ce84514dec805ec11993c5aa557a1af
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-07 20:16:28 -0300

    refactor: separete tests in describe blocks

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,14 @@
 import jwt from 'jsonwebtoken'
+import { Decrypter } from '../../../data/protocols/critography/decrypter'
 import { Encrypter } from '../../../data/protocols/critography/encrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (value: string): Promise<string> {
     const accessToken = await jwt.sign({ id: value }, this.secret)
     return accessToken
   }
+
+  decrypt: (value: string) => Promise<string>
 }

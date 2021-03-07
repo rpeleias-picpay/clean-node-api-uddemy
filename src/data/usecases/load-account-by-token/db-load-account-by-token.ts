3d6830fa41167d86e07eab21c7462fb89c743e9a
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-07 19:31:50 -0300

    feat: ensure DbLoadAccountByToken calls Decrypter with correct value

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -0,0 +1,13 @@
+import { resolve } from 'path'
+import { AccountModel } from '../../../domain/models/account'
+import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
+import { Decrypter } from '../../protocols/critography/decrypter'
+
+export class DbLoadAccountByToken implements LoadAccountByToken {
+  constructor (private readonly decrypter: Decrypter) {}
+
+  async load (accessToken: string, role?: string): Promise<AccountModel> {
+    await this.decrypter.decrypt(accessToken)
+    return null
+  }
+}

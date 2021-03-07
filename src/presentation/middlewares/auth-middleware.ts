935a743b09af06080ec807dd3def2ff86f58415d
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-07 18:58:47 -0300

    test: ensure AuthMiddleware return 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,17 +1,16 @@
-import { resolve } from "path";
-import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
-import { AccessDeniedError } from "../errors";
-import { forbidden } from "../helpers/http/http-helper";
-import { HttpRequest, HttpResponse, Middleware } from "../protocols";
+import { resolve } from 'path'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { AccessDeniedError } from '../errors'
+import { forbidden } from '../helpers/http/http-helper'
+import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
-    constructor(private readonly loadByAccountToken: LoadAccountByToken) {}
-    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const accessToken = httpRequest.headers?.['x-access-token']
-        if (accessToken) {
-            await this.loadByAccountToken.load(accessToken)
-        }
-        return forbidden(new AccessDeniedError())        
+  constructor (private readonly loadByAccountToken: LoadAccountByToken) {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = httpRequest.headers?.['x-access-token']
+    if (accessToken) {
+      await this.loadByAccountToken.load(accessToken)
     }
-
+    return forbidden(new AccessDeniedError())
+  }
 }

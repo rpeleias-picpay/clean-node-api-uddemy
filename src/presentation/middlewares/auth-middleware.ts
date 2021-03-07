490d07b7173a89a466eec4b4052325dff9787dc9
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-07 19:12:14 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct role

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,17 +1,19 @@
-import { resolve } from 'path'
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
-  constructor (private readonly loadByAccountToken: LoadAccountByToken) {}
+  constructor (
+    private readonly loadByAccountToken: LoadAccountByToken,
+    private readonly role?: string
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const accessToken = httpRequest.headers?.['x-access-token']
       if (accessToken) {
-        const account = await this.loadByAccountToken.load(accessToken)
+        const account = await this.loadByAccountToken.load(accessToken, this.role)
         if (account) {
           return ok({ accountId: account.id })
         }

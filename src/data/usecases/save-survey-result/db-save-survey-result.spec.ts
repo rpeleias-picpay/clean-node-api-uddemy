867872424fe606658dc127dffb4e72fd4250ea41
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-11 14:25:00 -0300

    test: ensure DbSaveSurveyResult returns a SurveyResult on a success

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -60,4 +60,10 @@ describe('DbSaveSurveyResult Usecase', () => {
     const promise = sut.save(surveyResultData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return SurveyReturn on success', async () => {
+    const { sut } = makeSut()
+    const surveyResult = await sut.save(makeFakeSurveyResultData())
+    expect(surveyResult).toEqual(makeFakeSurveyResult())
+  })
 })

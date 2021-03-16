97249af8a8f56e334a833f86516b9e377bf9a60c
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-16 09:04:14 -0300

    refactor: change save survey result return

--- src/domain/models/survey.ts
+++ src/domain/models/survey.ts
@@ -4,7 +4,8 @@ export interface SurveyModel {
   answers: SurveyAnswerModel[]
   date: Date
 }
-export interface SurveyAnswerModel {
+
+interface SurveyAnswerModel {
   image?: string
   answer: string
 }
--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -8,6 +8,19 @@ export const mockSurveyResultData = (): SaveSurveyResultParams => ({
   date: new Date()
 })
 
-export const mockSurveyResult = (): SurveyResultModel => Object.assign({}, mockSurveyResultData(), {
-  id: 'any_id'
+export const mockSurveyResult = (): SurveyResultModel => ({
+  surveyId: 'any_survey_id',
+  question: 'any_question',
+  answers: [{
+    answer: 'any_answer',
+    count: 1,
+    percent: 50
+  },
+  {
+    answer: 'other_answer',
+    image: 'any_image',
+    count: 10,
+    percent: 80
+  }],
+  date: new Date()
 })

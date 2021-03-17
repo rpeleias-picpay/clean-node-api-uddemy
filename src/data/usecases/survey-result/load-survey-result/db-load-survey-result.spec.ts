fe6b3a971baacc7cc524cd477af4b2c1bdecf511
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-17 09:40:49 -0300

    test: enure DbLoadSurveyResult reurns a surveyREsult on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -2,6 +2,7 @@ import { DbLoadSurveyResult } from './db-load-survey-result'
 import { mockLoadSurveyResultRepository } from '@/data/test'
 import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
 import { throwError } from '@/domain/test'
+import { mockSurveyResult } from '@/domain/test/mock-survey-result'
 
 interface SutTypes {
   sut: DbLoadSurveyResult
@@ -29,5 +30,11 @@ describe('DbLoadSurveyResult UseCase', () => {
     jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
     const promise = sut.load('any_survey_id')
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return SurveyResultModel on success', async () => {
+    const { sut } = makeSut()
+    const surveyResult = await sut.load('any_survey_id')
+    expect(surveyResult).toEqual(mockSurveyResult())
   })
 })

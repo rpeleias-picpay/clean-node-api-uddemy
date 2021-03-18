58ea49cba21274fb42f7f70192ee506566b085d6
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-18 09:41:03 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyByIdRepo if LoadSurveyResultRepo returns null

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,11 +1,17 @@
-import { LoadSurveyResultRepository, LoadSurveyResult } from './db-load-survey-result-protocols'
+import { LoadSurveyResultRepository, LoadSurveyResult, LoadSurveyByIdRepository } from './db-load-survey-result-protocols'
 import { SurveyResultModel } from '../save-survey-result/db-save-survey-result-protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+  constructor (
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {}
 
   async load (surveyId: string): Promise<SurveyResultModel> {
     const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    if (!surveyResult) {
+      await this.loadSurveyByIdRepository.loadById(surveyId)
+    }
     return surveyResult
   }
 }

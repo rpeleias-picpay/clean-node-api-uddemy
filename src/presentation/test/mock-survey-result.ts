3fc7ad33dfe44885aaeebd9af8cd47e70aee9c7e
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-20 13:05:05 -0300

    feat: ensue LoadSurveyResultController calls LoadSurveyREsult with correct value

--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -1,4 +1,5 @@
 import { mockSurveyResult } from '@/domain/test/mock-survey-result'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 import { SaveSurveyResult, SaveSurveyResultParams, SurveyResultModel } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols'
 
 export const mockSaveSurveyResult = (): SaveSurveyResult => {
@@ -8,4 +9,13 @@ export const mockSaveSurveyResult = (): SaveSurveyResult => {
     }
   }
   return new SaveSurveyResultStub()
+}
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    async load (surveyId: string): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResult())
+    }
+  }
+  return new LoadSurveyResultStub()
 }

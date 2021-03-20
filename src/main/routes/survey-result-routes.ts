f9cc02450a63b985931ffe8f447f5f0fae114a09
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-03-20 13:30:06 -0300

    feat: ensure loadsurvey route returns 403 if no accessToken is provided

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -2,8 +2,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '@/main/adapters/express-route-adapters'
 import { auth } from '@/main/middlewares/admin-auth'
-import { makeSaveSurveyResultController } from '../factories/controllers/survey-result/save-survey-result/save-survey-controller-factory'
+import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-controller-factory'
+import { makeLoadSurveyResultController } from '@/main/factories/controllers/survey-result/load-survey-result/load-survey-controller-factory'
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+  router.get('/surveys/:surveyId/results', auth, adaptRoute(makeLoadSurveyResultController()))
 }
